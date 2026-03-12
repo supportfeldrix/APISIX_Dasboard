acc4a7e114bf8912d3481fe576debdd0485692a2
Van Rooyen, Tienie <tienie.vanrooyen@fnb.co.za>
2026-03-12 11:21:53 +0200

    Edit podman_health.sh

--- playbooks/non-prod/health_checks/podman_health.sh
+++ playbooks/non-prod/health_checks/podman_health.sh
@@ -1,22 +1,21 @@
 #!/bin/bash
-#
-# Podman Health Check Cron Wrapper
-#
-
 set -e
 set -o pipefail
 
 ANSIBLE_BIN="/usr/bin/ansible-playbook"
-ANSIBLE_PLAYBOOK="/opt/podman/CROIT/playbooks/health_checks/pod-health.yml"
-INVENTORY_FILE="/opt/podman/CROIT/inventories/local.ini"
+PLAY_DIR="/opt/podman/CROIT/playbooks/health_checks"
+PLAYBOOK="$PLAY_DIR/pod-health.yml"
 LOG_FILE="/opt/podman/CROIT/playbooks/health_checks/podman_health.log"
 
-echo "===== $(date) Starting Podman health check =====" >> "$LOG_FILE"
+echo "===== $(date '+%F %T') START =====" >> "$LOG_FILE"
 
-$ANSIBLE_BIN \
-  -i "$INVENTORY_FILE" \
-  -e ansible_user=********* \
+cd "$PLAY_DIR"
+
+# Run against localhost with local connection (no SSH, no other user)
+"$ANSIBLE_BIN" \
+  -i localhost, \
+  -c local \
   -e notify_enabled=true \
-  "$ANSIBLE_PLAYBOOK" >> "$LOG_FILE" 2>&1
+  "$PLAYBOOK" >> "$LOG_FILE" 2>&1
 
-echo "===== $(date) Finished Podman health check =====" >> "$LOG_FILE"
+echo "===== $(date '+%F %T') END =====" >> "$LOG_FILE"
